e6794af1adafebb897226bbcf2f0e41585d9d6af
jiangy <jiangy@highzap.com>
2019-06-27 13:51:26 +0800

    添加自定义font icon

--- shopping_cart/lib/screens/home_screen.dart
+++ shopping_cart/lib/screens/home_screen.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_cart/app_icons.dart';
 
 /// 主页
 class HomeScreen extends StatelessWidget {
   /// 主页底部导航
   final List<BottomNavigationBarItem> items = [
-    BottomNavigationBarItem(icon: const Icon(Icons.home), title: Text("精选")),
-    BottomNavigationBarItem(icon: const Icon(Icons.home), title: Text("购物车")),
-    BottomNavigationBarItem(icon: const Icon(Icons.home), title: Text("我")),
+    BottomNavigationBarItem(
+        icon: const Icon(AppIcons.home), title: Text("精选")),
+    BottomNavigationBarItem(
+        icon: const Icon(AppIcons.cart), title: Text("购物车")),
+    BottomNavigationBarItem(
+        icon: const Icon(AppIcons.my), title: Text("我")),
   ];
 
   @override
@@ -15,6 +19,9 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: Text("商品信息"),
+        ),
+        body: Center(
+          child: Icon(AppIcons.home),
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: items,
